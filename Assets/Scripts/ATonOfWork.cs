294f4c42d6860833256d6fb100232b15f20c4e70
RangerHalt2 <141537190+RangerHalt2@users.noreply.github.com>
2025-09-10 05:10:53 -0400

    Dummy Commit again

---
+++ Assets/Scripts/ATonOfWork.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class ATonOfWork : MonoBehaviour
+{
+
+    //I'm doing a ton!
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
