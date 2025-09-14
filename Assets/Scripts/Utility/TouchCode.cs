3370380c070fc55894a53d0afc49df159358dab9
aknez615 <al683193@ucf.edu>
2025-09-13 23:00:46 -0400

    debugging

---
+++ Assets/Scripts/Utility/TouchCode.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class TouchCode : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        print(Input.touchCount);
+    }
+}
