9ea0b8958f016ff7a9f9393cd076d30e7b76a03c
aknez615 <al683193@ucf.edu>
2025-10-14 22:55:13 -0400

    update ItemHandler
    currently doing research on how to properly complete task

--- Assets/Scripts/Utility/EXP Systems/ItemHandler.cs
+++ Assets/Scripts/Utility/EXP Systems/ItemHandler.cs
@@ -1,7 +1,16 @@
+using System.Collections.Generic;
+using NUnit.Framework;
 using UnityEngine;
 
 public class ItemHandler : MonoBehaviour
 {
     //This should store variables and change their counts.
     //Public functions to get/set should be the core of this script.
+
+    public List<string> items;
+
+    public void Start()
+    {
+        items = new List<string>();
+    }
 }
