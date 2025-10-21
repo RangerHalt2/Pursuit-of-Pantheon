9d125b44069df5fe3429971fab7e9f8093c95405
aknez615 <al683193@ucf.edu>
2025-10-20 22:51:14 -0400

    scripts implemented just need testing with the other scripts

--- Assets/Scripts/Dialogue System/DialogueTrigger.cs
+++ Assets/Scripts/Dialogue System/DialogueTrigger.cs
@@ -1,13 +1,55 @@
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class DialogueTrigger : MonoBehaviour
 {
     public TextAsset dialogueJSON;
     public DialogueManager manager;
 
-    public void Start()
+    private bool dialogueStarted = false;
+
+    public void Update()
     {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (EventSystem.current != null && EventSystem.current.IsPointerOverGameObject()) return;
+
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.transform == this.transform)
+                {
+                    TriggerDialogue();
+                }
+            }
+        }
+    }
+
+    public void TriggerDialogue()
+    {
+        if (dialogueStarted) return;
+        dialogueStarted = true;
+
+        if (manager == null || dialogueJSON == null)
+        {
+            Debug.LogWarning("DialogueManager or dialogueJSON is not assigned");
+            return;
+        }
+
         Dialogue dialogue = manager.LoadDialogueFromJSON(dialogueJSON);
         manager.StartDialogue(dialogue);
     }
 }
+
+/*for future on calling this file use:
+ 
+ public DialogueTrigger npcDialogueTrigger;
+
+ void OnBattleWin()
+{
+    npcDialogueTrigger.TriggerDialogue();
+}
+
+ this script will be attached to NPC GameObjects that have colliders on them (set to trigger)*/
--- Assets/Scripts/Utility/EXP Systems/ItemHandler.cs
+++ Assets/Scripts/Utility/EXP Systems/ItemHandler.cs
@@ -1,5 +1,4 @@
 using System.Collections.Generic;
-using NUnit.Framework;
 using UnityEngine;
 
 public class ItemHandler : MonoBehaviour
@@ -7,10 +6,53 @@ public class ItemHandler : MonoBehaviour
     //This should store variables and change their counts.
     //Public functions to get/set should be the core of this script.
 
-    public List<string> items;
+    public static ItemHandler Instance;
 
-    public void Start()
+    private Dictionary<string, int> upgradeCounts = new Dictionary<string, int>();
+
+    private void Awake()
     {
-        items = new List<string>();
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+            LoadUpgrades(); //load saved data
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void GiveUpgrade(string upgradeID)
+    {
+        if (!upgradeCounts.ContainsKey(upgradeID))
+        {
+            upgradeCounts[upgradeID] = 0;
+        }
+
+        upgradeCounts[upgradeID]++;
+        PlayerPrefs.SetInt(upgradeID, upgradeCounts[upgradeID]);
+        PlayerPrefs.Save();
+    }
+
+    public int GetUpgradeCount(string upgradeID)
+    {
+        return upgradeCounts.ContainsKey(upgradeID) ? upgradeCounts[upgradeID] : 0;
+    }
+
+    private void LoadUpgrades()
+    {
+        string[] knownUpgrades = { "FollowerUpgrade" };
+
+        foreach (string id in knownUpgrades)
+        {
+            int count = PlayerPrefs.GetInt(id, 0);
+            upgradeCounts[id] = count;
+        }
     }
 }
+
+/*for the upgrades being given this needs to be called in the scripts
+ UpgradeManager.Instance.GiveUpgrade("FollowerUpgrade");
+The name of the upgrade can always be changed it is just a placeholder*/
